a42fecb2c5b94ceaafb73c971d3eefed68b82200
lnbcoding <leonardbui@gmail.com>
2015-01-27 20:27:32 -0800

    switch controllers to render html instead

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,25 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
 router.get('/', function(req, res) {
     res.redirect('/movies');
 });
 
 router.get('/movies', function(req, res) {
-    res.render('search', {
+    res.render('search.html', {
         title: "Da Mean Movie Dashboard"
     });
 });
 
 router.get('/movies/popular', function(req, res) {
-    res.render('movies', {
+    res.render('movies.html', {
         title: "Da Mean Movie Dashboard"
     });
 });
 
 router.get('/movie/search', function(req, res) {
-    res.render('movie', {
+    res.render('movie.html', {
         title: "Da Mean Movie Dashboard"
     });
 });
