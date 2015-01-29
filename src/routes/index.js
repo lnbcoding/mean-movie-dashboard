61781f17bd163cd05e75300e52af1839965fe758
lnbcoding <leonardbui@gmail.com>
2015-01-29 13:38:58 -0800

    clean up express routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
 
-/* GET home page. */
+/********** Start of Routes used in Simple Movie Search ***********************/
 router.get('/', function(req, res) {
     res.redirect('/movies');
 });
@@ -12,6 +13,8 @@ router.get('/movies', function(req, res) {
     });
 });
 
+/********** End of Routes used in Simple Movie Search ***********************/
+
 router.get('/movies/popular', function(req, res) {
     res.render('movies.html', {
         title: "Da Mean Movie Dashboard"
@@ -19,8 +22,35 @@ router.get('/movies/popular', function(req, res) {
 });
 
 router.get('/movie/search', function(req, res) {
+    var movieTitle = req.params('title');
+    var movieYear = req.params('year');
+    var searchURL = "http://www.omdbapi.com/?t="+movieTitle+"&y="+movieYear+"&plot=short&r=json"
+
+    request('http://www.google.com', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Print the google web page.
+        }
+    })
+
+    var options = {
+        host: 'www.google.com',
+        port: 80,
+        // path: '/upload',
+        method: 'GET'
+    };
+
+    var req = http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    });
+
     res.render('movie.html', {
-        title: "Da Mean Movie Dashboard"
+        title: "Da Mean Movie Dashboard",
+        // searchResult: JSON.parse(xhr.response),
     });
 });
 
